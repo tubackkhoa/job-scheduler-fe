817242207b27186748fe1becd9f8da8ec7706393
Pham Tu <tubackkhoa@gmail.com>
2026-01-21 07:08:29 +0700

    import jinja as raw

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ import { linter, Diagnostic } from '@codemirror/lint';
 import { syntaxTree } from '@codemirror/language';
 import _ from 'lodash';
 import api from './api';
+import jinja from './jinja.py?raw';
 
 export const getCodeHash = (str: string) => {
   let hash = 5381;
@@ -363,69 +364,9 @@ export const jinjaLinter = (
 const initPyodide: Promise<PyodideAPI> = (async () => {
   // @ts-ignore
   const pyodide: PyodideAPI = await loadPyodide();
-
   // Ensure Jinja2 is available
   await pyodide.loadPackage('jinja2');
-
-  // Define Python code
-  await pyodide.runPythonAsync(`
-from jinja2.sandbox import SandboxedEnvironment
-from jinja2 import meta
-import inspect
-import json
-
-def tolist(obj, *include):
-    if include:
-        include_set = set(include)
-        return [{k: v for k, v in item.to_dict().items() if k in include_set} for item in obj]
-    return [item.to_dict() for item in obj]
-
-sandbox = SandboxedEnvironment(autoescape=False, trim_blocks=True, lstrip_blocks=True)
-sandbox.filters.update({
-    "in_clause": lambda values: (
-        "()" if not values else f"({','.join(map(repr, values))})"
-    ),
-    "tolist": tolist,
-})
-
-def describe_callable(obj):    
-    if callable(obj):
-        try:
-            signature = str(inspect.signature(obj))
-        except (ValueError, TypeError):
-            signature = None
-
-        return {
-            "type": "function",
-            "doc": inspect.getdoc(obj),
-            "signature": signature,
-        }
-
-    try:
-        cls = obj if isinstance(obj, type) else type(obj)
-        doc = f"{cls.__module__}.{cls.__qualname__}"
-    except Exception:
-        doc = str(obj)
-
-    return {
-        "type": "variable",
-        "doc": doc,
-    }
-
-doc_json = json.dumps({
-  "globals": {name: describe_callable(value) for name, value in sandbox.globals.items()},
-  "filters": {name: describe_callable(value) for name, value in sandbox.filters.items()},
-  "tests": tuple(sandbox.tests),
-  "tags": [tag for ext in sandbox.extensions.values() for tag in getattr(ext, "tags", ())],
-})
-
-def render(tpl_str, context):
-    try:                
-        return sandbox.from_string(tpl_str).render(context, this=context)
-    except Exception:        
-        ast = sandbox.parse(tpl_str)
-        return meta.find_undeclared_variables(ast)
-  `);
+  await pyodide.runPythonAsync(jinja);
   console.log('Pyodide initialized');
   return pyodide;
 })();
