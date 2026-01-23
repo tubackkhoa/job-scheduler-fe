6f412baf6aae357116a1bcd1e5e407d5e553eb30
Pham Tu <tubackkhoa@gmail.com>
2026-01-23 17:08:23 +0700

    fix adapter

--- src/components/ConfigForm.tsx
+++ src/components/ConfigForm.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 import Form from '@rjsf/mui';
 import validator from '@rjsf/validator-ajv8';
 import { extractUiSchema, buildUiSchemaWithExpr } from '@/utils';
-import * as fields from './fields';
+import fields from './fields';
 import widgets from './widgets';
 import { ErrorBoundary } from './ErrorBound';
 import { ObjectFieldTemplate } from './templates/ObjectFieldTemplate';
--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -1,5 +1,47 @@
-export { SelectField as Select } from './SelectField';
-export { TemplateField as Template } from './TemplateField';
-export { VersionField as Version } from './VersionField';
-export { MLThresholdsTableField as MLThresholdsTable } from './MLThresholdsTableField';
-export { default as Dynamic } from './DynamicField';
+import { FieldProps } from '@rjsf/utils';
+import DynamicField from './DynamicField';
+import { MLThresholdsTableField } from './MLThresholdsTableField';
+import { SelectField } from './SelectField';
+import { TemplateField } from './TemplateField';
+import { VersionField } from './VersionField';
+import { FC } from 'react';
+
+export default {
+  MLThresholdsTable: MLThresholdsTableField,
+  Select: SelectField,
+  Template: TemplateField,
+  Version: VersionField,
+  Dynamic: DynamicField
+};
+
+/* ----------------------------------
+ * Simplified props for normal React
+ * ---------------------------------- */
+export type FieldComponentProps = Pick<
+  FieldProps,
+  'schema' | 'uiSchema' | 'formData' | 'onChange'
+>;
+
+/* ----------------------------------
+ * Adapter: RJSF field -> normal component
+ * ---------------------------------- */
+export function adaptField<T extends FC<FieldProps>>(Field: T) {
+  return (props: FieldComponentProps) =>
+    Field({
+      ...props,
+      onBlur: undefined,
+      onFocus: undefined,
+      fieldPathId: undefined,
+      name: '',
+      registry: undefined
+    } as FieldProps);
+}
+
+/* ----------------------------------
+ * Normal React exports (adapted)
+ * ---------------------------------- */
+export const Select = adaptField(SelectField);
+export const Template = adaptField(TemplateField);
+export const Version = adaptField(VersionField);
+export const MLThresholdsTable = adaptField(MLThresholdsTableField);
+export const Dynamic = adaptField(DynamicField);
