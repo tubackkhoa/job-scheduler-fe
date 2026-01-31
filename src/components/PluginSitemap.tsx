77a54fcaa4f9a8fce4940ddbeccf5d589ba1c18c
Pham Tu <tubackkhoa@gmail.com>
2026-01-31 15:26:50 +0700

    add specific file for libs

--- src/components/PluginSitemap.tsx
+++ src/components/PluginSitemap.tsx
@@ -10,13 +10,7 @@ import {
   Collapse,
   Tooltip,
 } from '@mui/material';
-import {
-  Edit,
-  ExpandLess,
-  ExpandMore,
-  Extension,
-  Route,
-} from '@mui/icons-material';
+import { ChevronRight, Edit, Extension, Route } from '@mui/icons-material';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useEffect, useState, useCallback } from 'react';
 import api from '@/api';
@@ -105,10 +99,8 @@ export function PluginSitemap() {
 
               {isLoading ? (
                 <CircularProgress size={16} />
-              ) : isOpen ? (
-                <ExpandLess />
-              ) : (
-                <ExpandMore />
+              ) : isOpen ? null : (
+                <ChevronRight />
               )}
             </ListItemButton>
 
