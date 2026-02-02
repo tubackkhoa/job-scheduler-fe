d6513829d2b08c01b7c3d6d6da5d2931667a75fb
Pham Tu <tubackkhoa@gmail.com>
2026-02-02 20:47:39 +0700

    add theme component

--- src/components/fields/JavascriptPreview.tsx
+++ src/components/fields/JavascriptPreview.tsx
@@ -1,7 +1,14 @@
 import { Console } from 'console-feed';
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-import { Box, Paper, Typography, Stack, Divider } from '@mui/material';
+import {
+  Box,
+  Paper,
+  Typography,
+  Stack,
+  Divider,
+  useTheme,
+} from '@mui/material';
 
 interface Props {
   text: string;
@@ -16,7 +23,7 @@ const runCode = (
   iframe: HTMLIFrameElement,
   code: string,
   providers: string[],
-  hideConsole = true
+  hideConsole = true,
 ) => {
   iframe.srcdoc = `
 <script>
@@ -72,8 +79,9 @@ const runCode = (
 export const JavascriptPreview: React.FC<Props> = ({
   text,
   fullscreen,
-  providers
+  providers,
 }) => {
+  const theme = useTheme();
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const [logs, setLogs] = useState<any[]>([]);
 
@@ -87,8 +95,8 @@ export const JavascriptPreview: React.FC<Props> = ({
         ...prev,
         {
           method: event.data.method,
-          data: event.data.args
-        }
+          data: event.data.args,
+        },
       ]);
     };
 
@@ -108,7 +116,7 @@ export const JavascriptPreview: React.FC<Props> = ({
         maxHeight: fullscreen ? '100%' : 600,
         minHeight: 300,
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
       }}
     >
       <iframe ref={iframeRef} style={{ display: 'none' }} />
@@ -134,10 +142,10 @@ export const JavascriptPreview: React.FC<Props> = ({
           px: 1,
           py: 0.5,
           fontFamily: 'Roboto Mono, monospace',
-          fontSize: 13
+          fontSize: 13,
         }}
       >
-        <Console logs={logs} variant="dark" />
+        <Console logs={logs} variant={theme.palette.mode} />
       </Box>
     </Paper>
   );
