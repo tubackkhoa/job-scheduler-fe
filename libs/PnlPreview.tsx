ff2ea1c563e981ca0eb2aeddc5c0fdaa98f77237
Pham Tu <tubackkhoa@gmail.com>
2026-02-06 11:39:40 +0700

    fix pnl responsive

--- libs/PnlPreview.tsx
+++ libs/PnlPreview.tsx
@@ -1141,7 +1141,12 @@ export default ({ formData, registry }: FieldProps) => {
       </Grid>
 
       {/* Toolbar */}
-      <Stack direction="row" spacing={2} sx={{ mb: 2 }} alignItems="center">
+      <Stack
+        direction={{ md: 'row', xs: 'column' }}
+        gap={2}
+        sx={{ mb: 2 }}
+        alignItems="center"
+      >
         <TextField
           size="small"
           placeholder="Search..."
@@ -1174,7 +1179,7 @@ export default ({ formData, registry }: FieldProps) => {
             setStatusFilter(e.target.value);
             setPage(0);
           }}
-          sx={{ width: 50, flex: 1 }}
+          sx={{ flex: 1 }}
         >
           <MenuItem value="All">All</MenuItem>
           <MenuItem value="active">Active</MenuItem>
@@ -1239,7 +1244,7 @@ export default ({ formData, registry }: FieldProps) => {
         <Table size="small">
           <TableHead>
             <TableRow sx={{ bgcolor: 'action.hover' }}>
-              <TableCell sx={{ width: 40 }} padding="none" />
+              <TableCell padding="none" />
               {activeColumns.map((key) => (
                 <TableCell key={key} sx={{ fontWeight: 600 }}>
                   <TableSortLabel
