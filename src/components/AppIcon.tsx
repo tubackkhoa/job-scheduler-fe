06767522ac5550223d2905477558e13faeafb2b1
Pham Tu <tubackkhoa@gmail.com>
2026-02-09 08:39:14 +0700

    cleaner and faster code for AppIcon

--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@mui/material';
 import * as MuiIcons from '@mui/icons-material';
 import React from 'react';
+import _ from 'lodash';
 
 type IconProps = React.ComponentProps<typeof Icon>;
 type AppIconName = keyof typeof MuiIcons;
@@ -21,10 +22,7 @@ const LIGATURE_EXCEPTIONS: Partial<Record<AppIconName, string>> = {
 export default new Proxy({} as AppIconType, {
   get(target, key: AppIconName) {
     return (target[key] ??= (props: IconProps) => (
-      <Icon {...props}>
-        {LIGATURE_EXCEPTIONS[key] ??
-          key.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase()}
-      </Icon>
+      <Icon {...props}>{LIGATURE_EXCEPTIONS[key] ?? _.snakeCase(key)}</Icon>
     ));
   },
 });
