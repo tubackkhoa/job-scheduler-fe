c0f28a7a5149192e9c4dae9055ee867f1cb9c4c9
Pham Tu <tubackkhoa@gmail.com>
2026-02-09 08:23:16 +0700

    cleaner and faster code for AppIcon

--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -2,28 +2,29 @@ import { Icon } from '@mui/material';
 import * as MuiIcons from '@mui/icons-material';
 import React from 'react';
 
-type MuiIconName = keyof typeof MuiIcons;
-
-function toLigature(name: string) {
-  return name.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
-}
-
 type IconProps = React.ComponentProps<typeof Icon>;
-
-// Base renderer
-function RenderIcon({ name, ...props }: { name: MuiIconName } & IconProps) {
-  return <Icon {...props}>{toLigature(name)}</Icon>;
-}
+type AppIconName = keyof typeof MuiIcons;
 
 // Build a typed proxy object
 type AppIconType = {
-  [K in MuiIconName]: React.FC<IconProps>;
+  [K in AppIconName]: React.FC<IconProps>;
+};
+
+const LIGATURE_EXCEPTIONS: Partial<Record<AppIconName, string>> = {
+  ThreeDRotation: '3d_rotation',
+  ThreeSixty: '360',
+  FourK: '4k',
+  FiveG: '5g',
+  OneKk: '10k',
 };
 
 export default new Proxy({} as AppIconType, {
-  get(_, key: string) {
-    return (props: IconProps) => (
-      <RenderIcon name={key as MuiIconName} {...props} />
-    );
+  get(target, key: AppIconName) {
+    return (target[key] ??= (props: IconProps) => (
+      <Icon {...props}>
+        {LIGATURE_EXCEPTIONS[key] ??
+          key.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase()}
+      </Icon>
+    ));
   },
 });
